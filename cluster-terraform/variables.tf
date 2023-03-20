731807bab9b2fdc81b895a82827c0b62ea70d570
Afuape Esther <108690181+Folakunmi21@users.noreply.github.com>
2023-03-20 09:29:57 +0100

    Update variables.tf

--- cluster-terraform/variables.tf
+++ cluster-terraform/variables.tf
@@ -1,10 +1,4 @@
-variable "accesskey" {
-    default = "AKIAY5XQYB2IVVPKVABT"
-}
 
-variable "secretkey" {
-    default = "T1eBF7yyvOSDncsjmA8ycVNWl0cjMgeayc8WjWHY"
-}
  
 variable "cluster-name" {
     default = "eks-cluster"
